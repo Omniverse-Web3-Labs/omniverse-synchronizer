afba20d3b5683d718d891c6977e83c3732ddc0e3
virgil2019 <danten0402@gmail.com>
2022-09-18 22:55:36 +0800

    Data structure and interface

--- contracts/contracts/OmniverseProtocol.sol
+++ contracts/contracts/OmniverseProtocol.sol
@@ -27,7 +27,40 @@ contract OmniverseProtocol is IOmniverseProtocol {
     /**
      * @dev See IOmniverseProtocl
      */
-    function verifyTxSignature() external override returns (bool) {
+    function verifyTxSignature(OmniverseTokenProtocol calldata _data) external override returns (bool) {
+        RecordedCertificate storage rc = transactionRecorder[_data.from];
+        // Check nonce
+        if (rc.nonce == _data.nonce) {
+            bytes32 hash = getTransactionHash(_data);
+        }
+        else if (rc.nonce > _data.nonce) {
 
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * @dev Get the hash of a tx
+     */
+    function getTransactionHash(OmniverseTokenProtocol calldata _data) internal returns (bytes32) {
+        bytes memory rawData = abi.encodePacked(uint128(_data.nonce), _data.chainId, _data.from, _data.to, _data.data);
+        return keccak256(rawData);
+    }
+
+    /**
+     * @dev Recover the address
+     */
+    function recoverAddress(bytes32 _hash, bytes memory _signature) internal returns (address) {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        assembly {
+            r := mload(add(_signature, 32))
+            s := mload(add(_signature, 64))
+            v := mload(add(_signature, 65))
+        }
+        return ecrecover(_hash, v, r, s);
     }
 }
--- contracts/contracts/SkyWalkerFungible.sol
+++ contracts/contracts/SkyWalkerFungible.sol
@@ -6,5 +6,34 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./interfaces/IOmniverseProtocol";
 
 contract SkyWalkerFungible is ERC20, Ownable {
-    
+    mapping(string => string) members;
+    mapping(bytes => uint256) omniverseBalances;
+    mapping(bytes => uint256) prisons;
+    mapping(bytes => uint256) pending;
+
+    /**
+     * @dev Transfer omniverse tokens to a user
+     */
+    function omniverseTransfer(OmniverseTokenProtocol calldata _data) public {
+
+    }
+
+    /**
+     * @dev Approve omniverse tokens for a user
+     */
+    function omniverseApprove(OmniverseTokenProtocol calldata _data) public {
+    }
+
+    /**
+     * @dev Transfer omniverse tokens from a user to another user
+     */
+    function omniverseTransferFrom(OmniverseTokenProtocol calldata _data) public {
+    }
+
+    /**
+     * @dev Returns the omniverse balance of a user
+     */
+    function omniverseBalanceOf(bytes calldata _pk) public returns (uint256) {
+        return omniverseBalances[_pk];
+    }
 }
