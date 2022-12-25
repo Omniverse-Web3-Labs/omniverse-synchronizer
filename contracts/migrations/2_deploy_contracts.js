eba4b89a2e63768a73b3a53aa0118aa55d7298e2
virgil2019 <danten0402@gmail.com>
2022-12-26 07:15:25 +0800

    Update deploy script

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -3,20 +3,20 @@ const SkywalkerFungible = artifacts.require("SkywalkerFungible");
 const fs = require("fs");
 
 module.exports = async function (deployer, network) {
-  await deployer.deploy(OmniverseProtocol, 0);
+  const contractAddressFile = './config/default.json';
+  let data = fs.readFileSync(contractAddressFile, 'utf8');
+  let jsonData = JSON.parse(data);
+  if (!jsonData[network]) {
+    console.error('There is no config for: ', network, ', please add.');
+    return;
+  }
+
+  await deployer.deploy(OmniverseProtocol, jsonData[network].omniverseChainId);
   await deployer.deploy(SkywalkerFungible, "Skywalker", "SW", "SW");
 
   // Update config
   if (network.indexOf('-fork') != -1 || network == 'test' || network == 'development') {
     return;
-  }
-
-  const contractAddressFile = './config/default.json';
-  let data = fs.readFileSync(contractAddressFile, 'utf8');
-  let jsonData = JSON.parse(data);
-  if (!jsonData[network]) {
-    console.warn('There is no config for: ', network, ', please add.');
-    jsonData[network] = {};
   }
 
   jsonData[network].omniverseProtocolAddress = OmniverseProtocol.address;
