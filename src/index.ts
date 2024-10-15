0062e9aeec3418a87206cbc799d051fdf670796c
virgil2019 <danten0402@gmail.com>
2024-10-15 11:03:17 +0800

    chore: change storage path of .state

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import config from 'config';
 import { LocalSigner } from './signer/localSigner';
 
 async function main() {
-  const state = new LocalStorage('./.state');
+  const state = new LocalStorage('./storage/.state');
   const server = new OmniverseServer(config.get('omniverseServer.rpc'));
   const signer = new LocalSigner(config.get('secret'));
   const synchronizer = new SynchronizerMain(signer, state, server);
